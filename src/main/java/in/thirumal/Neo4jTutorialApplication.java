c1298f27d2587a3300a36a1bdd98b8224a8ff836
Thirumal <m.thirumal@hotmail.com>
2021-10-06 15:25:54 +0530

    r

--- src/main/java/in/thirumal/Neo4jTutorialApplication.java
+++ src/main/java/in/thirumal/Neo4jTutorialApplication.java
@@ -2,11 +2,7 @@ package in.thirumal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.repository.config.EnableReactiveNeo4jRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
-@EnableReactiveNeo4jRepositories
 @SpringBootApplication
 public class Neo4jTutorialApplication {
 
