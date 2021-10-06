4d975a1b8c38b1be7c72a11b1951f919ca21f795
Thirumal <m.thirumal@hotmail.com>
2021-10-06 14:14:28 +0530

    first commit

---
+++ src/main/java/in/thirumal/Neo4jTutorialApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jTutorialApplication.class, args);
+	}
+
+}
